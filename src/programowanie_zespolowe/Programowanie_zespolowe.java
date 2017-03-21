36ba065960f61215d546e9242318208661a7f972
Gomex <Gomex@Gomex-Komputer.local>
2017-03-21 19:07:46 +0100

    Panel Pracownika

--- src/programowanie_zespolowe/Programowanie_zespolowe.java
+++ src/programowanie_zespolowe/Programowanie_zespolowe.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programowanie_zespolowe;
 
 import javafx.application.Application;
@@ -19,19 +14,23 @@ public class Programowanie_zespolowe extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
+        Stage pracownikStage = new Stage();
+        
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root1 = FXMLLoader.load(getClass().getResource("FXMLPracownik.fxml"));
         
         Scene scene = new Scene(root);
+        Scene scene1 = new Scene(root1);
         
         stage.setScene(scene);
+        pracownikStage.setTitle("Panel Pracownika");
+        pracownikStage.setScene(scene1);
+        
         stage.show();
+        pracownikStage.show();
     }
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
