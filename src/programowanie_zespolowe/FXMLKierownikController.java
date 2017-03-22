0085696d274c7c4d9ec8a5462c5105b2bd06adae
Homik <Homik@172.27.24.27>
2017-03-22 22:24:17 +0100

    Gui v2

--- src/programowanie_zespolowe/FXMLKierownikController.java
+++ src/programowanie_zespolowe/FXMLKierownikController.java
@@ -5,12 +5,19 @@
  */
 package programowanie_zespolowe;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  *
@@ -18,7 +25,51 @@ import javafx.scene.control.Label;
  */
 public class FXMLKierownikController implements Initializable {
     
-    
+    @FXML
+    private Button zadanie;
+    @FXML
+    private Button samochod;
+    @FXML
+    private Button przydziel;
+    @FXML
+    private void dodajZadanie(ActionEvent event) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("dodajZadanie.fxml"));
+        Scene edit_scene = new Scene(load);
+        Stage edit_stage = new Stage();
+        edit_stage.setTitle("Dodaj Zadanie");
+        edit_stage.setScene(edit_scene);
+        edit_stage.setResizable(false);
+        edit_stage.sizeToScene();
+        edit_stage.initModality(Modality.APPLICATION_MODAL);
+        edit_stage.initOwner(zadanie.getScene().getWindow());
+        edit_stage.showAndWait();
+    }
+    @FXML
+    private void dodajSamochod(ActionEvent event) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("dodajSamochod.fxml"));
+        Scene edit_scene = new Scene(load);
+        Stage edit_stage = new Stage();
+        edit_stage.setTitle("Dodaj Samochod");
+        edit_stage.setScene(edit_scene);
+        edit_stage.setResizable(false);
+        edit_stage.sizeToScene();
+        edit_stage.initModality(Modality.APPLICATION_MODAL);
+        edit_stage.initOwner(samochod.getScene().getWindow());
+        edit_stage.showAndWait();
+    }
+    @FXML
+    private void przydzielZadanie(ActionEvent event) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("przydzielZadanie.fxml"));
+        Scene edit_scene = new Scene(load);
+        Stage edit_stage = new Stage();
+        edit_stage.setTitle("Przydziel pracownika");
+        edit_stage.setScene(edit_scene);
+        edit_stage.setResizable(false);
+        edit_stage.sizeToScene();
+        edit_stage.initModality(Modality.APPLICATION_MODAL);
+        edit_stage.initOwner(przydziel.getScene().getWindow());
+        edit_stage.showAndWait();
+    }
     
     
     
--- src/programowanie_zespolowe/FXML_LogowanieController.java
+++ src/programowanie_zespolowe/FXML_LogowanieController.java
@@ -8,7 +8,6 @@ package programowanie_zespolowe;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,7 +18,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import static javax.management.Query.value;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 /**
