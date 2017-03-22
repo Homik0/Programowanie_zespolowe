5f327783eaf8fa9a70b9a3f4a0b1e9d5459ba812
Doktor3k <kawkaa9@gmail.com>
2017-03-22 13:29:40 +0100

    Dodany panel dyrektora

--- src/programowanie_zespolowe/Programowanie_zespolowe.java
+++ src/programowanie_zespolowe/Programowanie_zespolowe.java
@@ -15,19 +15,25 @@ public class Programowanie_zespolowe extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Stage pracownikStage = new Stage();
-        
+        Stage dyrektorStage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         Parent root1 = FXMLLoader.load(getClass().getResource("FXMLPracownik.fxml"));
-        
+        Parent root2 = FXMLLoader.load(getClass().getResource("FXMLDyrektor.fxml"));
         Scene scene = new Scene(root);
         Scene scene1 = new Scene(root1);
+        Scene scene2 = new Scene(root2);
         
         stage.setScene(scene);
         pracownikStage.setTitle("Panel Pracownika");
         pracownikStage.setScene(scene1);
-        
         stage.show();
         pracownikStage.show();
+        
+        stage.setScene(scene);
+        dyrektorStage.setTitle("Panel Dyrektora");
+        dyrektorStage.setScene(scene2);
+        stage.show();
+        dyrektorStage.show();
     }
     
     public static void main(String[] args) {
